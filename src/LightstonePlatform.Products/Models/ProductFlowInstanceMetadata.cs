8f4c24e4f8c428eb653977124e36069be1bb2400
Armand van der Walt <armandw@lsproperty.onmicrosoft.com>
2022-07-06 10:34:44 +0200

    Add quote meta data

--- src/LightstonePlatform.Products/Models/ProductFlowInstanceMetadata.cs
+++ src/LightstonePlatform.Products/Models/ProductFlowInstanceMetadata.cs
@@ -3,5 +3,7 @@
     public class ProductFlowInstanceMetadata
     {
         public string Source { get; set; }
+        public QuoteDisplayModel Quote { get; set; }
+        public string AdditionalConfiguration { get; set; }
     }
 }
---
+++ src/LightstonePlatform.Products/Models/QuoteDisplayModel.cs
@@ -0,0 +1,36 @@
+﻿using LightstonePlatform.Products.Models;
+using System;
+using System.Collections.Generic;
+
+public class QuoteDisplayModel
+{
+    public Guid? UniqueReference
+    {
+        get;
+        set;
+    }
+
+    public IEnumerable<QuoteLineItemDisplayModel> LineItems
+    {
+        get;
+        set;
+    }
+
+    public decimal VAT
+    {
+        get;
+        set;
+    }
+
+    public decimal Total
+    {
+        get;
+        set;
+    }
+
+    public string Status
+    {
+        get;
+        set;
+    }
+}
