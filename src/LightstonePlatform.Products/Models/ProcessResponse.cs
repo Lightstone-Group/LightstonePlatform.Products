111550ba0f51f55b834c3dc76206ff063ebb3626
Armand <armandvdwalt1@gmail.com>
2022-07-14 12:26:53 +0200

    Product description update (#11)
    * Add support to update product description as part of process method
    
    Co-authored-by: Armand van der Walt <armandw@lsproperty.onmicrosoft.com>

--- src/LightstonePlatform.Products/Models/ProcessResponse.cs
+++ src/LightstonePlatform.Products/Models/ProcessResponse.cs
@@ -6,5 +6,6 @@ namespace LightstonePlatform.Products.Models
     {
         public virtual ResponseType Type { get; set; }
         public object AdditionalData { get; set; }
+        public string ProductDescription { get; set; }
     }
 }
