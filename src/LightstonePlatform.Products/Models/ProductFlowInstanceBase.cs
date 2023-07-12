7608873d98d519394222bb913dd5da81a67f802a
Jacques Snyman <jacques@jacquessnyman.co.za>
2023-07-12 14:34:07 +0200

    fix(model): remove special character from Json Property name (#14)

--- src/LightstonePlatform.Products/Models/ProductFlowInstanceBase.cs
+++ src/LightstonePlatform.Products/Models/ProductFlowInstanceBase.cs
@@ -4,7 +4,7 @@ namespace LightstonePlatform.Products.Models
 {
     public class ProductFlowInstanceBase
     {
-        [JsonProperty("Ïd")]
+        [JsonProperty("Id")]
         public string ProductFlowId { get; set; }
         public ProductFlowInstanceContext Context { get; set; } = new ProductFlowInstanceContext();
         public ProductFlowInstanceMetadata Metadata { get; set; } = new ProductFlowInstanceMetadata();
