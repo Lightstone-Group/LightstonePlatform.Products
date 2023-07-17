72e3d5f9b3bc747c2898c4fb1396c8a091f222e5
Ewald Stieger <e.stieger@gmail.com>
2023-07-17 16:19:34 +0200

    FIX: Decorate ProductFlowInstanceBase.ProductFlowId for Text.Json (#15)
    * FIX: Decorate ProductFlowInstanceBase.ProductFlowId for Text.Json serialization
    
    * FIX: Add namespace System.Text.Json.Serialization

--- src/LightstonePlatform.Products/Models/ProductFlowInstanceBase.cs
+++ src/LightstonePlatform.Products/Models/ProductFlowInstanceBase.cs
@@ -1,10 +1,12 @@
 ﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace LightstonePlatform.Products.Models
 {
     public class ProductFlowInstanceBase
     {
         [JsonProperty("Id")]
+        [JsonPropertyName("Id")]
         public string ProductFlowId { get; set; }
         public ProductFlowInstanceContext Context { get; set; } = new ProductFlowInstanceContext();
         public ProductFlowInstanceMetadata Metadata { get; set; } = new ProductFlowInstanceMetadata();
