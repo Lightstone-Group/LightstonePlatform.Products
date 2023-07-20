7fe6d4da2db116648b2dacfd0faa5b07fe892f4c
Ewald Stieger <e.stieger@gmail.com>
2023-07-20 15:57:35 +0200

    FIX: Remove JSON decorators and add SourceDataUrl property

--- src/LightstonePlatform.Products/Models/ProductFlowInstanceBase.cs
+++ src/LightstonePlatform.Products/Models/ProductFlowInstanceBase.cs
@@ -5,9 +5,8 @@ namespace LightstonePlatform.Products.Models
 {
     public class ProductFlowInstanceBase
     {
-        [JsonProperty("Id")]
-        [JsonPropertyName("Id")]
         public string ProductFlowId { get; set; }
+        public string SourceDataUrl { get; set; }
         public ProductFlowInstanceContext Context { get; set; } = new ProductFlowInstanceContext();
         public ProductFlowInstanceMetadata Metadata { get; set; } = new ProductFlowInstanceMetadata();
     }
