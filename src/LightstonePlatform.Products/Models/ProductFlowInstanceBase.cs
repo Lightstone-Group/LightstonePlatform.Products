e9b08384c4c34b6500b6ac71faece631b094d56b
Armand <armandvdwalt1@gmail.com>
2024-07-24 13:31:56 +0200

    fix(process-model): add missing properties for process model (#21)
    Co-authored-by: Armand van der Walt <=>

--- src/LightstonePlatform.Products/Models/ProductFlowInstanceBase.cs
+++ src/LightstonePlatform.Products/Models/ProductFlowInstanceBase.cs
@@ -10,5 +10,7 @@ namespace LightstonePlatform.Products.Models
         public ProductFlowInstanceContext Context { get; set; } = new ProductFlowInstanceContext();
         public ProductFlowInstanceMetadata Metadata { get; set; } = new ProductFlowInstanceMetadata();
         public string CustomizationHash { get; set; }
+        public string PartyId { get; set; }
+        public string TenantId { get; set; }
     }
 }
