1b7200a694e010d98c594520d60b2069df164bf5
tiaantLightstone <96467432+tiaantLightstone@users.noreply.github.com>
2025-05-16 13:41:44 +0200

    feat: added HandleError endpoint with response models (#23)

---
+++ src/LightstonePlatform.Products/Models/ProductFlowInstanceHandleErrorModel.cs
@@ -0,0 +1,14 @@
+﻿using LightstonePlatform.Products.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightstonePlatform.Products.Models
+{
+    public class ProductFlowInstanceHandleErrorModel
+    {
+        public string AdditionalConfiguration { get; set; }
+        public Guid TenantId { get; set; }
+        public ProductFlowInstanceStatus ProductFlowInstanceStatus { get; set; }
+    }
+}
---
+++ src/LightstonePlatform.Products/Models/ShowUIHandleErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using LightstonePlatform.Products.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightstonePlatform.Products.Models
+{
+    [Serializable]
+    public class ShowUIHandleErrorResponse : HandleErrorResponse
+    {
+        public override ResponseType Type { get => ResponseType.ShowUI; set => base.Type = value; }
+        public string ElementUrl { get; set; }
+        public string ElementName { get; set; }
+        public Dictionary<string, string> Attributes { get; set; }
+        public object Body { get; set; }
+    }
+}
