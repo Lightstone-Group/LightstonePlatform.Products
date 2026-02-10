c78a549d8b47d495978cc597c4f062e9af20a91c
Armand <armandvdwalt1@gmail.com>
2026-02-10 14:52:48 +0200

    fix: error input model definitiion (#36)
    * fix: add generic type param
    
    * fix: update HandleError method parameter to use ProductFlowInstanceErrorInputModel
    
    * fix: change ProductFlowId type from Guid to string in ProductFlowInstanceErrorInputModel
    
    ---------
    
    Co-authored-by: Armand van der Walt <armandw@lightstone.co.za>

---
+++ src/LightstonePlatform.Products/Models/ProductFlowInstanceErrorInputModel.cs
@@ -0,0 +1,12 @@
+﻿using LightstonePlatform.Products.Enums;
+using System;
+
+namespace LightstonePlatform.Products.Models
+{
+    public class ProductFlowInstanceErrorInputModel
+    {
+        public string ProductFlowId { get; set; }
+        public string PartyId { get; set; }
+        public ProductFlowInstanceStatus Status { get; set; }
+    }
+}
