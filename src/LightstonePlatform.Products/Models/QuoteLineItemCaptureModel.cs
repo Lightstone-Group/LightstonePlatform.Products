09feaec73430abd0cde8afa2c459f5f5e1da8af1
Tiaan Du Toit <tiaant@lsproperty.onmicrosoft.com>
2026-07-08 08:05:58 +0200

    feat: Add LineItems property and QuoteLineItemCaptureModel class
    Added a `LineItems` property to the `ProductFlowInstanceMetadata`
    class to store a collection of `QuoteLineItemCaptureModel` objects.
    Introduced the `QuoteLineItemCaptureModel` class with properties
    for description, quantity, product code, and an optional unique
    reference. Updated necessary `using` directives to support the
    new class and its functionality.

---
+++ src/LightstonePlatform.Products/Models/QuoteLineItemCaptureModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightstonePlatform.Products.Models
+{
+    [Serializable]
+    public class QuoteLineItemCaptureModel
+    {
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string ProductCode { get; set; }
+        public Guid? UniqueReference { get; set; }
+    }
+}
